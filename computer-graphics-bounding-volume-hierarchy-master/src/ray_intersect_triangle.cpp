acd9b114a7a0b06b0bc52b0e6051b2d006ccd811
roger <roger.l.jiahao2@gmail.com>
2022-02-09 19:51:27 -0500

    A4 starter

---
+++ computer-graphics-bounding-volume-hierarchy-master/src/ray_intersect_triangle.cpp
@@ -0,0 +1,45 @@
+#include "ray_intersect_triangle.h"
+
+bool ray_intersect_triangle(
+  const Ray & ray,
+  const Eigen::RowVector3d & A,
+  const Eigen::RowVector3d & B,
+  const Eigen::RowVector3d & C,
+  const double min_t,
+  const double max_t,
+  double & t)
+{
+  ////////////////////////////////////////////////////////////////////////////
+  Eigen::Vector3d a = std::get<0>(Triangle::corners);
+  Eigen::Vector3d b = std::get<1>(Triangle::corners);
+  Eigen::Vector3d c = std::get<2>(Triangle::corners);
+
+  Eigen::Vector3d e1 = b - a;
+  Eigen::Vector3d e2 = c - a;
+  Eigen::Vector3d s = ray.origin - a;
+  Eigen::Vector3d s1 = ray.direction.cross(e2);
+  Eigen::Vector3d s2 = s.cross(e1);
+
+  Eigen::Vector3d temp;
+  temp << s2.dot(e2), s1.dot(s), s2.dot(ray.direction);
+
+  Eigen::Vector3d unknowns;
+  unknowns << (1.0/(s1.dot(e1))) * (temp);
+
+  if (unknowns(1) >= 0 && unknowns(2) >= 0 && (unknowns(1) + unknowns(2)) <= 1) {
+    if (unknowns(0) >= min_t) {
+      t = unknowns(0);
+
+
+      Eigen::Vector3d vec1 = b - a;
+      Eigen::Vector3d vec2 = c - a;
+      n = (vec1).cross(vec2);
+      n = n.normalized();
+      return true;
+    }
+  }
+
+  return false;
+  ////////////////////////////////////////////////////////////////////////////
+}
+
